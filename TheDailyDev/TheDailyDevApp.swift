313bb34200187861c258d5e479e46eec753421a2
Claire Knutson <claireknutson@MacBook-Pro.attlocal.net>
2025-10-15 20:37:31 -0500

    Initial commit

---
+++ TheDailyDev/TheDailyDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheDailyDevApp.swift
+//  TheDailyDev
+//
+//  Created by Claire Knutson on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TheDailyDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
